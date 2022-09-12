65069342fe5ecc06a9eee5e0e451e19b56a85c29
Joseph Timothy Foley <foley@ru.is>
2022-09-12 18:10:53 +0000

    started interfacing with excel spreadsheet

--- applygrading.py
+++ applygrading.py
@@ -5,3 +5,13 @@
 ## Input: Spreadsheet, List of Variables "Name=", penalty and credit codes e.g. "INT1(-2)" and "!MISC"
 ## In the future:  This list will be supplied by the PDF comment extractor
 ## Output:  Updated Spreadsheet
+## Ubuntu install
+##   sudo apt install python3-openpyxl
+## Doc: https://openpyxl.readthedocs.io/en/stable/tutorial.html 
+from openpyxl import load_workbook
+wb = load_workbook("templates/notebook-eval.xlsx")
+#print(wb.sheetnames)
+mysheet = wb["team1"]
+
+# STUB:  We know that codes are in column A
+# STUB:  We know that we need to update values in column C
