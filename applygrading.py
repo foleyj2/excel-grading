d1ccbabc4227a873a45956b2f7daf7958fb1828c
Joseph Timothy Foley <foley@ru.is>
2022-09-12 17:59:15 +0000

    Starting development of applygrading

---
+++ applygrading.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+## Take a list of grading codes and update it in the Rubric-style grading sheet
+## Author:  Joseph T. Foley <foley AT RU DOT IS>
+## Start Date: 2022-09-12
+## Input: Spreadsheet, List of Variables "Name=", penalty and credit codes e.g. "INT1(-2)" and "!MISC"
+## In the future:  This list will be supplied by the PDF comment extractor
+## Output:  Updated Spreadsheet
